31f9f57f383f40c255d6d5d28e3832465a55783e
Nick Johnson <arachnid@notdot.net>
2014-03-17 14:26:09 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="mcp2210",
+      version="0.1",
+      description="Python interface for the MCP2210 USB-SPI interface",
+      author="Nick Johnson",
+      author_email="nick@arachnidlabs.com",
+      url="https://github.com/arachnidlabs/mcp2210/",
+      packages=["mcp2210"],
+      requires=["hidapi"])
