35a0692856abc7d08ab82db029368a894a5cfe78
Nick Johnson <arachnid@notdot.net>
2014-09-26 21:59:45 +0100

    Fixed deps

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name="mcp2210",
-      version="0.1.3",
+      version="0.1.4",
       description="Python interface for the MCP2210 USB-SPI interface",
       author="Nick Johnson",
       author_email="nick@arachnidlabs.com",
       url="https://github.com/arachnidlabs/mcp2210/",
       packages=["mcp2210"],
-      requires=["hidapi"])
+      install_requires=["hidapi>=0.7.99"])
